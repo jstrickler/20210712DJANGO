96402ada5239266c9d97a5bc839ad316ca12a239
jstrickler <jstrickler@gmail.com>
2021-07-14 08:02:09 -0400

    replaced djform

--- EXAMPLES/djangoexamples/djform/superheroes/urls.py
+++ EXAMPLES/djangoexamples/djform/superheroes/urls.py
@@ -4,11 +4,7 @@ URL Configuration for superheroes
 from django.urls import path
 from . import views
 
-<<<<<<< HEAD
-app_name = "superheroes"
-=======
 app_name = 'superheroes'
->>>>>>> 31ee024df05c1bbba52ee7853427671b3002c6ae
 
 urlpatterns = [
     path('', views.home, name='home'),
