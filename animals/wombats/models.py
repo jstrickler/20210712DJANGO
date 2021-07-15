9169b3a5256b08c11c87bf1170d60aae9f213027
jstrickler <jstrickler@gmail.com>
2021-07-15 07:53:20 -0400

    End of Wednesday

--- animals/wombats/models.py
+++ animals/wombats/models.py
@@ -8,7 +8,13 @@ class Wombat(models.Model):
     shootable = models.BooleanField(default=False)
     age = models.PositiveSmallIntegerField()
     speed = models.FloatField(default=0.0)
+    attitude = models.CharField(max_length=32, null=False)
     location = models.CharField(max_length=64)
+    sex = models.CharField(
+        max_length=1,
+        choices=[('m', 'male'), ('f', 'female')],
+        null=False,
+    )
 
     def __str__(self):
         return f"{self.name} ({self.location})"
